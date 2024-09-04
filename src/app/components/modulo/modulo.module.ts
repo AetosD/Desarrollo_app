cae0024b317b4f18f6ceb4545ff99e22953a957d
D.Osorio <88414076+AetosD@users.noreply.github.com>
2024-09-04 16:02:27 -0400

    commit

---
+++ src/app/components/modulo/modulo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PieComponent } from '../pie/pie.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    PieComponent,
+  ]
+})
+export class ModuloModule { }
--- src/app/pages/page404/page404.module.ts
+++ src/app/pages/page404/page404.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { Page404PageRoutingModule } from './page404-routing.module';
-
 import { Page404Page } from './page404.page';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatNativeDateModule } from '@angular/material/core'
-
+import { ModuloModule } from 'src/app/components/modulo/modulo.module';
 @NgModule({
   imports: [
     CommonModule,
@@ -17,7 +14,8 @@ import { MatNativeDateModule } from '@angular/material/core'
     IonicModule,
     Page404PageRoutingModule,
     MatSlideToggleModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    ModuloModule
   ],
   declarations: [Page404Page]
 })
